e8ad537c439fde0390be56b3783e2b032b597b51
jack.hua <jack.hua@gosomo.cn>
2017-03-25 11:13:25 +0800

    重新提交未保存的修改

--- src/com/db/CoolWeatherOpenHelper.java
+++ src/com/db/CoolWeatherOpenHelper.java
@@ -1,8 +1,64 @@
 package com.db;
 
-public class CoolWeatherOpenHelper     {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CoolWeatherOpenHelper     extends SQLiteOpenHelper{
+	
+	public static final String TableName_Province = "Province";
+	public static final String TableName_City = "City";
+	public static final String TableName_County = "County";
 	
 	
 	
+	//Province建表语句
+	public static final String CREATE_PROVINCE = "create table Province("
+			+ "id integer primary key autoincrement, "
+			+ "province_name text, "
+			+ "province_code text)";
+	
+	//City建表语句
+	public static final String CREATE_CITY = "create table City("
+			+ "id integer primary key autoincrement, "
+			+ "city_name text, "
+			+ "city_code text,"
+			+ "province_id integer)";
+	
+	//County建表语句
+	public static final String CREATE_COUNTY = "create table County("
+			+ "id integer primary key autoincrement, "
+			+ "county_name text, "
+			+ "county_code text, "
+			+ "city_id integer)";
+	
+		
+	//有参构造函数
+	public CoolWeatherOpenHelper(Context context, String name, CursorFactory factory, int version) {
+		super(context, name, factory, version);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(CREATE_PROVINCE);	
+		db.execSQL(CREATE_CITY);
+		db.execSQL(CREATE_COUNTY);
+		
+	}
+	
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			
+		}
 
 }
--- src/com/model/City.java
+++ src/com/model/City.java
@@ -1,5 +1,46 @@
 package com.model;
 
 public class City {
-
+	
+	private int id;
+	private String cityName;
+	private String cityCode;
+	private int provindeId;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getCityName() {
+		return cityName;
+	}
+	
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	
+	public String getCityCode() {
+		return cityCode;
+	}
+	
+	
+	public void setCityCode(String cityCode) {
+		this.cityCode = cityCode;
+	}
+	
+	
+	public int getProvindeId() {
+		return provindeId;
+	}
+	
+	public void setProvindeId(int provindeId) {
+		this.provindeId = provindeId;
+	}
+	
+	
+	
 }
